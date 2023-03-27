70e5519251c703a0b14bfb52b51719fb27eb43ff
saurabhkurve <saurabhkurve100@gmail.com>
2023-03-27 19:59:45 +0530

    Logic corrected!

--- Week 2/Week 2.1/third.cpp
+++ Week 2/Week 2.1/third.cpp
@@ -15,7 +15,7 @@ int main(){
     if(year%400==0){
         cout<<"This is a leap year"<<endl;
     }else if(year%100==0){
-        cout<<"This is a leap year"<<endl;
+        cout<<"This is not a leap year"<<endl;
    }else if(year%4==0){  
         cout<<"This is a leap year"<<endl;
     }else{
